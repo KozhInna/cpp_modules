6be4c86bc0f1b41c69b7f966a376c3bee8b7fa40
KozhInna <inna.kozhina22@gmail.com>
2025-10-29 12:58:52 +0200

    general setup of the project

---
+++ module_00/ex01/Phonebook.class.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Phonebook.class.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ikozhina <ikozhina@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/29 12:45:06 by ikozhina          #+#    #+#             */
+/*   Updated: 2025/10/29 12:57:58 by ikozhina         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHONEBOOK_CLASS_HPP
+# define PHONEBOOK_CLASS_HPP
+
+# include "Contact.class.hpp"
+
+class Phonebook {
+    
+    Contact contacts[8];
+};
+
+#endif
