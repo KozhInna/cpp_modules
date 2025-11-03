3ae28ce226022ebc7c68abcf57d4722b5fbb6f2c
KozhInna <inna.kozhina22@gmail.com>
2025-11-03 11:26:53 +0200

    getline fail fix

--- module_00/ex01/Contact.cpp
+++ module_00/ex01/Contact.cpp
@@ -6,7 +6,7 @@
 /*   By: ikozhina <ikozhina@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/29 12:17:47 by ikozhina          #+#    #+#             */
-/*   Updated: 2025/10/31 12:37:37 by ikozhina         ###   ########.fr       */
+/*   Updated: 2025/11/03 10:55:04 by ikozhina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,50 +14,50 @@
 
 std::string Contact::getFirstName() const
 {
-    return firstName;  
-} 
+	return firstName;
+}
 
 std::string Contact::getLastName() const
 {
-    return lastName;
+	return lastName;
 }
-    
+
 std::string Contact::getNickname() const
 {
-    return nickname;
+	return nickname;
 }
 
 std::string Contact::getPhoneNumber() const
 {
-    return phoneNumber;
+	return phoneNumber;
 }
 
 std::string Contact::getDarkestSecret() const
 {
-    return darkestSecret;
+	return darkestSecret;
 }
 
 void Contact::setFirstName(const std::string& name)
 {
-    firstName = name;
+	firstName = name;
 }
 
 void Contact::setLastName(const std::string& name)
 {
-    lastName = name;
+	lastName = name;
 }
 
 void Contact::setNickname(const std::string& name)
 {
-    nickname = name;
+	nickname = name;
 }
 
 void Contact::setPhoneNumber(const std::string& number)
 {
-    phoneNumber = number;
+	phoneNumber = number;
 }
 
 void Contact::setDarkestSecret(const std::string& secret)
 {
-    darkestSecret = secret;
+	darkestSecret = secret;
 }
--- module_00/ex01/PhoneBook.cpp
+++ module_00/ex01/PhoneBook.cpp
@@ -1,65 +1,63 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   phonebook.cpp                                      :+:      :+:    :+:   */
+/*   PhoneBook.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ikozhina <ikozhina@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/31 15:51:15 by ikozhina          #+#    #+#             */
-/*   Updated: 2025/11/02 17:53:06 by ikozhina         ###   ########.fr       */
+/*   Updated: 2025/11/03 11:03:27 by ikozhina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PhoneBook.hpp"
-#include <iostream>
-#include <iomanip>
 
 static std::string truncate(const std::string& str)
 {
-    if (str.length() > 10)
-    {
-        return (str.substr(0, 9) + ".");
-    }
-    return str;
+	if (str.length() > 10)
+	{
+		return (str.substr(0, 9) + ".");
+	}
+	return str;
 }
 
 void PhoneBook::addContact(const Contact& contact)
 {
-    contacts[nextIndex] = contact;
-    nextIndex = (nextIndex + 1) % 8;
-    if (numContacts < 8)
-        numContacts++;
+	contacts[nextIndex] = contact;
+	nextIndex = (nextIndex + 1) % 8;
+	if (numContacts < 8)
+		numContacts++;
 }
 
 void PhoneBook::displayContacts()
 {
-    std::cout << std::setw(10) << std::right << "Index" << "|"
-              << std::setw(10) << std::right << "First name" << "|"
-              << std::setw(10) << std::right << "Last name" << "|"
-              << std::setw(10) << std::right << "Nickname" << std::endl;
-    for (int i = 0; i < numContacts; i++)
-    {
-        std::cout << std::setw(10) << std::right << (i + 1) << "|"
-        << std::setw(10) << std::right << truncate(contacts[i].getFirstName()) << "|"
-        << std::setw(10) << std::right << truncate(contacts[i].getLastName()) << "|"
-        << std::setw(10) << std::right << truncate(contacts[i].getNickname()) << std::endl;
-    }
+	std::cout << std::setw(10) << std::right << "Index" << "|"
+			  << std::setw(10) << std::right << "First name" << "|"
+			  << std::setw(10) << std::right << "Last name" << "|"
+			  << std::setw(10) << std::right << "Nickname" << std::endl;
+	for (int i = 0; i < numContacts; i++)
+	{
+		std::cout << std::setw(10) << std::right << (i + 1) << "|"
+		<< std::setw(10) << std::right << truncate(contacts[i].getFirstName()) << "|"
+		<< std::setw(10) << std::right << truncate(contacts[i].getLastName()) << "|"
+		<< std::setw(10) << std::right << truncate(contacts[i].getNickname()) << std::endl;
+	}
 }
 
 void PhoneBook::displayContactInfo(int index)
 {
-    std::cout << "First name: ";
-    std::cout << contacts[index - 1].getFirstName() << std::endl;
-    std::cout << "Last name: ";
-    std::cout << contacts[index - 1].getLastName() << std::endl;
-    std::cout << "Nickname: ";
-    std::cout << contacts[index - 1].getNickname() << std::endl;
-    std::cout << "Phone number: ";
-    std::cout << contacts[index - 1].getPhoneNumber() << std::endl;
-    std::cout << "Darkest secret: ";
-    std::cout << contacts[index - 1].getDarkestSecret() << std::endl;
-}      
+	std::cout << "First name: ";
+	std::cout << contacts[index - 1].getFirstName() << std::endl;
+	std::cout << "Last name: ";
+	std::cout << contacts[index - 1].getLastName() << std::endl;
+	std::cout << "Nickname: ";
+	std::cout << contacts[index - 1].getNickname() << std::endl;
+	std::cout << "Phone number: ";
+	std::cout << contacts[index - 1].getPhoneNumber() << std::endl;
+	std::cout << "Darkest secret: ";
+	std::cout << contacts[index - 1].getDarkestSecret() << std::endl;
+}
 int PhoneBook::getNumContacts() const
 {
-    return numContacts;
+	return numContacts;
 }
