a48532ee49248db35d0a7e400e74bae188f7d771
KozhInna <inna.kozhina22@gmail.com>
2025-12-08 19:44:14 +0200

    add tests to main

--- module_04/ex00/main.cpp
+++ module_04/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ikozhina <ikozhina@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/04 15:09:16 by ikozhina          #+#    #+#             */
-/*   Updated: 2025/12/08 14:45:14 by ikozhina         ###   ########.fr       */
+/*   Updated: 2025/12/08 19:40:18 by ikozhina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,20 +20,21 @@
 int main()
 {
 	{
+        std::cout << "*** With virtual ***" << std::endl;
 		std::cout << "=== Constructors ===" << std::endl;
 		const Animal* meta = new Animal();
 		const Animal* dog = new Dog();
 		const Animal* cat = new Cat();
 
 		std::cout << "\n=== Make sound ===" << std::endl;
-		std::cout << "Type > " << dog->getType() << " " << std::endl;
+		std::cout << "Type > " << dog->getType() << " | ";
 		dog->makeSound();
 
-		std::cout << "Type > " << cat->getType() << " " << std::endl;
+		std::cout << "Type > " << cat->getType() << " | ";
 		cat->makeSound();
 
-		std::cout << "Type > " << meta->getType() << " " << std::endl;
-		meta->makeSound(); //generic sound
+		std::cout << "Type > " << meta->getType() << " | ";
+		meta->makeSound();
 
 		std::cout << "\n=== Destructors ===" << std::endl;
 		delete dog;
@@ -41,11 +42,26 @@ int main()
 		delete meta;
 	}
 	{
-		std::cout << "\n=== Wrong animal ===" << std::endl;
-		const WrongAnimal* wrongCat = new WrongCat();
-		std::cout << wrongCat->getType() << std::endl;
-		wrongCat->makeSound();
-		delete wrongCat;
+        std::cout << "\n*** Without virtual ***" << std::endl;
+		std::cout << "=== Pointer to base class ===" << std::endl;
+		const WrongAnimal* wrongCat1 = new WrongCat();
+        const WrongAnimal* wrongCat2 = new WrongAnimal();
+        
+        std::cout << "\nType > " << wrongCat1->getType() << " | ";
+		wrongCat1->makeSound();
+        std::cout << "Type > " << wrongCat2->getType() << " | ";
+		wrongCat2->makeSound();
+        
+        std::cout << std::endl;
+		delete wrongCat1;
+        delete wrongCat2;
 	}
+    {
+        std::cout << "\n=== Direct object call ===" << std::endl;
+        WrongCat directWrongCat;
+        std::cout << "\nType > " << directWrongCat.getType() << " | ";
+		directWrongCat.makeSound();
+        std::cout << std::endl;
+    }
 	return 0;
 }
